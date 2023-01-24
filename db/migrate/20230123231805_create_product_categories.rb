5517d499be7395ed184ef7e7ea83df1a314e1a5d
Danilo Guimaraes <guimaraes.louie@gmail.com>
2023-01-24 09:49:14 +0000

    Forgot to save some stuff

--- db/migrate/20230123231805_create_product_categories.rb
+++ db/migrate/20230123231805_create_product_categories.rb
@@ -1,8 +1,8 @@
 class CreateProductCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :product_categories do |t|
-      t.references :product, null: false, foreign_key: true
-      t.references :category, null: false, foreign_key: true
+      t.references :product, foreign_key: true
+      t.references :category, foreign_key: true
 
       t.timestamps
     end
