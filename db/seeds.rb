05a9d1ba3507412e164f07f05cad103463b4bc88
Danilo Guimaraes <guimaraes.louie@gmail.com>
2023-01-25 14:33:53 +0000

    Seeds added to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,175 +10,175 @@
 
 Product.create(
   name: 'Electra Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with metallic silver fabric and gray details, in a futuristic style. Perfect for a night out!',
   price: 38.99,
-  cltag: 'AI-SK8/sneaker_.jpg'
+  cltag: 'AI-SK8/sneaker_1.jpg'
 )
 
 Product.create(
   name: 'Thomas Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with black rubbery fabric and orange, blue and beige details, in a retro style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_2.jpg'
 )
 
 Product.create(
   name: 'Ash Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 36.99,
+  description: 'A chunky sneaker with black glossy fabric, in a futuristic style.',
+  price: 38.99,
   cltag: 'AI-SK8/sneaker_3.jpg'
 )
 
 Product.create(
   name: 'Jacob Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with gray fabric and beige details, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_4.jpg'
 )
 
 Product.create(
   name: 'Kitty Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with light pink fabric and pink details, in a casual style.',
+  price: 37.99,
   cltag: 'AI-SK8/sneaker_5.jpg'
 )
 
 Product.create(
   name: 'Margot Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with red fabric and dark red details, in a futuristic style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_6.jpg'
 )
 
 Product.create(
   name: 'Flamma Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with orange fabric and gray details, in a sporty style.',
+  price: 37.99,
   cltag: 'AI-SK8/sneaker_7.jpg'
 )
 
 Product.create(
   name: 'Tropicalia Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with salmon-pink fabric and light pink details, in a sporty style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_8.jpg'
 )
 
 Product.create(
   name: 'Indigo Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with dark blue fabric and black details, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_9.jpg'
 )
 
 Product.create(
   name: 'Tangerine Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with white fabric and orange details, in a sporty style.',
+  price: 36.99,
   cltag: 'AI-SK8/sneaker_10.jpg'
 )
 
 Product.create(
   name: 'Faith Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with white fabric and black and dark pink details, in a retro style.',
+  price: 35.99,
   cltag: 'AI-SK8/sneaker_11.jpg'
 )
 
 Product.create(
   name: 'Calli Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with dark pink fabric and orange details, in a sporty style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_12.jpg'
 )
 
 Product.create(
   name: 'Rosé Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with light pink fabric, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_13.jpg'
 )
 
 Product.create(
   name: 'Tiger Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with light pink fabric and pink, gray and black details, in a futuristic style.',
+  price: 37.99,
   cltag: 'AI-SK8/sneaker_14.jpg'
 )
 
 Product.create(
   name: 'Sunny Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with yellow fabric and white details, in a casual style.',
+  price: 36.99,
   cltag: 'AI-SK8/sneaker_15.jpg'
 )
 
 Product.create(
   name: 'Hope Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with light pink fabric and white details, in a casual style.',
+  price: 37.99,
   cltag: 'AI-SK8/sneaker_16.jpg'
 )
 
 Product.create(
   name: 'Zapp Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with black rubbery fabric and gold details, in a futuristic style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_17.jpg'
 )
 
 Product.create(
   name: 'Cynthia Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with light green fabric and white and gray details, in a sporty style.',
+  price: 32.99,
   cltag: 'AI-SK8/sneaker_18.jpg'
 )
 
 Product.create(
   name: 'Elohra Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with white fabric and black and gray details, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_19.jpg'
 )
 
 Product.create(
   name: 'Matthes Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with black fabric and light green details, in a sporty style.',
+  price: 32.99,
   cltag: 'AI-SK8/sneaker_20.jpg'
 )
 
 Product.create(
   name: 'Shakx Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with black rubbery fabric and dark pink details, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_21.jpg'
 )
 
 Product.create(
   name: 'Lionne Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with orange fabric and light pink and brown details, in a futuristic style.',
+  price: 37.99,
   cltag: 'AI-SK8/sneaker_22.jpg'
 )
 
 Product.create(
   name: 'Topaz Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
-  price: 34.99,
+  description: 'A chunky sneaker with white fabric and black and yellow details, in a futuristic style.',
+  price: 36.99,
   cltag: 'AI-SK8/sneaker_23.jpg'
 )
 
 Product.create(
   name: 'Pearl Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with white fabric, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_24.jpg'
 )
 
 Product.create(
   name: 'Lizz Sneaker',
-  description: 'A chunky sneaker with _ fabric and _ details, of the _ style. Perfect for _!',
+  description: 'A chunky sneaker with brown fabric and black details, in a casual style.',
   price: 34.99,
   cltag: 'AI-SK8/sneaker_25.jpg'
 )
